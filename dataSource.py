50b1c2b164ef342a1ddc524582e802740182a1cc
unknown <othman.alkhatib.en@gmail.com>
2022-04-22 02:55:55 +0300

    Modified

---
+++ dataSource.py
@@ -0,0 +1,60 @@
+import psycopg2
+import logging
+
+logger = logging.getLogger()
+
+ADD_USER_STATEMENT = """UPDATE count
+                                SET users = users + 1 """
+
+
+class DataSource:
+    def __init__(self, database_url):
+        self.database_url = database_url
+
+    def get_connection(self):
+        return psycopg2.connect(self.database_url, sslmode="allow")
+
+    @staticmethod
+    def close_connection(conn):
+        if conn is not None:   # finally Statement Below Will Always be Applied, so I Need This Because Sometimes There is an Error in Connection
+            conn.close()
+
+    def create_tables(self):
+        commands = (
+            """
+                CREATE TABLE IF NOT EXISTS count (
+                    users INT NOT NULL
+                )
+            """,)
+
+        conn = None
+        try:
+            conn = self.get_connection()
+            cur = conn.cursor()   # Boss That Make Changes and Commands on Database
+            for command in commands:   # Because It is Multi Line of Commands
+                cur.execute(command)
+            cur.close()
+            conn.commit()   # Apply Edits
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.error(error)
+            raise error
+
+        finally:
+            self.close_connection(conn)
+
+    def add_user(self):
+        conn = None
+        try:
+            conn = self.get_connection()
+            cur = conn.cursor()
+            cur.execute(ADD_USER_STATEMENT)
+            cur.close()
+            conn.commit()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.error(error)
+            raise error
+
+        finally:
+            self.close_connection(conn)
--- main.py
+++ main.py
@@ -1,23 +1,78 @@
 from telegram.ext import CommandHandler, Updater, ConversationHandler, MessageHandler, Filters, CallbackContext
 from telegram import (KeyboardButton, ReplyKeyboardMarkup, Update)
-# from dataSource import DataSource
+from dataSource import DataSource
 import os
-# import threading
-# import time
-# import datetime
 import logging
 import sys
 
 
 READER_DATA = {
-    "أنس بادي":
+    "القارئ أنس بادي":
         {
-            "سورة الكهف كاملة": "https://t.me/ahsan_alhadeeth/75"
+            "سورة الكهف كاملة": ["https://t.me/ahsan_alhadeeth/75", "https://t.me/ahsan_alhadeeth/76"],
+            "سورة الملك كاملة": ["https://t.me/ahsan_alhadeeth/39", "https://t.me/ahsan_alhadeeth/40"],
+            "سورة الإنسان كاملة": ["https://t.me/mstoda3/120", "https://t.me/mstoda3/121"],
+            "دعاء رمضان 1442": ["https://t.me/mstoda3/98", "https://t.me/mstoda3/99"],
+            "من سورة لقمان": ["https://t.me/mstoda3/91", "https://t.me/mstoda3/92"],
         },
     "الشيخ محمد المصري":
        {
-           "مقتطف من سورة الصافات": "https://t.me/ahsan_alhadeeth/104",
-           "مقتطف من سورة الأنعام": "https://t.me/ahsan_alhadeeth/67"
+            "من سورة الصافات": ["https://t.me/mstoda3/126", "https://t.me/mstoda3/124"],
+            "من سورة الأنعام 2": ["https://t.me/ahsan_alhadeeth/67", "https://t.me/ahsan_alhadeeth/68"],
+            "من سورة ص" : ["https://t.me/ahsan_alhadeeth/33","https://t.me/ahsan_alhadeeth/34"],
+            "من سورة الأحزاب" : ["https://t.me/mstoda3/117","https://t.me/mstoda3/118"],
+            "من سورة الإسراء" : ["https://t.me/mstoda3/105","https://t.me/mstoda3/106"],
+            "من سورة الرعد" : ["https://t.me/mstoda3/85","https://t.me/mstoda3/86"],
+            "من سورة الأعراف" : ["https://t.me/mstoda3/70","https://t.me/mstoda3/73"],
+            "من سورة الأنعام": ["https://t.me/mstoda3/59", "https://t.me/mstoda3/75"],
+        },
+    "القارئ خالد جيزاوي":
+        {
+            "سورة الطور كاملة": ["https://t.me/mstoda3/128", "https://t.me/mstoda3/139"],
+            "من سورة الواقعة": ["https://t.me/ahsan_alhadeeth/62", "https://t.me/ahsan_alhadeeth/63"],
+            "من سورة الفرقان": ["https://t.me/ahsan_alhadeeth/20", "https://t.me/ahsan_alhadeeth/21"],
+        },
+    "القارئ عدنان الجودي":
+        {
+            "من سورة البقرة": ["https://t.me/ahsan_alhadeeth/83", "https://t.me/ahsan_alhadeeth/84"],
+            "من سورة آل عمران": ["https://t.me/ahsan_alhadeeth/42", "https://t.me/ahsan_alhadeeth/43"],
+            "من سورة الإسراء": ["https://t.me/mstoda3/114", "https://t.me/mstoda3/115"],
+            "من سورة ابراهيم": ["https://t.me/mstoda3/101", "https://t.me/mstoda3/102"],
+        },
+    "الشيخ محمد أنس علوش":
+        {
+            "من سورة مريم": ["https://t.me/mstoda3/127", "https://t.me/mstoda3/125"],
+            "من سورة الحجرات": ["https://t.me/mstoda3/108", "https://t.me/mstoda3/109"],
+            "من سورة القصص": ["https://t.me/mstoda3/88", "https://t.me/mstoda3/89"],
+            "من سورة النمل": ["https://t.me/mstoda3/79", "https://t.me/mstoda3/80"],
+            "من سورة طه": ["https://t.me/mstoda3/63", "https://t.me/mstoda3/76"],
+        },
+    "الشيخ صفوح الكيلاني":
+        {
+            "من سورة يونس": ["https://t.me/mstoda3/147", "https://t.me/mstoda3/148"],
+            "من سورة يس": ["https://t.me/ahsan_alhadeeth/28", "https://t.me/ahsan_alhadeeth/29"],
+            "سورة المنافقون كاملة": ["https://t.me/mstoda3/111", "https://t.me/mstoda3/112"],
+            "من سورة مريم": ["https://t.me/mstoda3/95", "https://t.me/mstoda3/96"],
+            "من سورة يوسف": ["https://t.me/mstoda3/82", "https://t.me/mstoda3/83"],
+        },
+    "الشيخ محمود مطر":
+        {
+            "من سورة ابراهيم": ["https://t.me/mstoda3/149", "https://t.me/mstoda3/150"],
+            "من سورة المؤمنون": ["https://t.me/ahsan_alhadeeth/93", "https://t.me/ahsan_alhadeeth/94"],
+            "من سورة طه": ["https://t.me/ahsan_alhadeeth/47", "https://t.me/ahsan_alhadeeth/48"],
+            "من سورة مريم": ["https://t.me/ahsan_alhadeeth/11", "https://t.me/ahsan_alhadeeth/13"],
+        },
+    "القارئ نبراس العبدالله":
+        {
+            "من سورة الصافات": ["https://t.me/ahsan_alhadeeth/71", "https://t.me/ahsan_alhadeeth/72"],
+            "من سورة القصص": ["https://t.me/ahsan_alhadeeth/16", "https://t.me/ahsan_alhadeeth/17"],
+        },
+    "الشيخ نادر الدباغ":
+        {
+            "سورة ق كاملة": ["https://t.me/mstoda3/151", "https://t.me/mstoda3/152"],
+            "من سورة القصص": ["https://t.me/ahsan_alhadeeth/99", "https://t.me/ahsan_alhadeeth/100"],
+            "من سورة الأنبياء": ["https://t.me/ahsan_alhadeeth/51", "https://t.me/ahsan_alhadeeth/52"],
+            "من سورة مريم": ["https://t.me/ahsan_alhadeeth/24", "https://t.me/ahsan_alhadeeth/25"],
         }
 }
 
@@ -29,6 +84,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 MODE = os.getenv("MODE")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+data_source = DataSource(DATABASE_URL)
 logger = logging.getLogger()
 
 if MODE == "dev":
@@ -46,10 +103,18 @@ else:
 
 def start_handler(update, context):
     # Reply With Next Step (Buttons)
-    update.message.reply_text("حياك الله في قناة أحسن الحديث, الرجاء اختيار قارئ", reply_markup=reader_buttons())
+    update.message.reply_text("""
+    حياك الله في بوت قناة أحسن الحديث ❤️
+    
+    لا تنسَ الاشتراك في قناتنا الرسمية على تلغرام عبر الرابط التالي : https://t.me/ahsan_alhadeeth
+    لتبقى على اطلاع بأحدث المنشورات والتلاوات العطرة في القناة
 
-def back(update: Update, context: CallbackContext):
-    update
+    """)
+    update.message.reply_text("الرجاء اختيار قارئ", reply_markup=reader_buttons())
+    data_source.add_user()
+
+def goBack(update: Update, context: CallbackContext):
+    update.message.reply_text("الرجاء اختيار قارئ", reply_markup=reader_buttons())
 
 def reader_info(reader):
     return list(READER_DATA[reader].keys())
@@ -75,10 +140,9 @@ def reader_video_buttons(update: Update, context: CallbackContext) :
 def reader_video_button_handler(update: Update, context: CallbackContext) :
     reader_name = context.user_data["reader_name"]
     telawa = update.message.text
-    update.message.bot.send_video(update.message.chat_id, READER_DATA[reader_name][telawa])
+    update.message.bot.send_video(update.message.chat_id, READER_DATA[reader_name][telawa][0])
+    update.message.bot.send_video(update.message.chat_id, READER_DATA[reader_name][telawa][1])
 
-def choose_telawa(update: Update, context: CallbackContext):
-    update.message.reply_text("اختر تلاوة", reply_markup=reader_video_buttons)
 
 if __name__ == "__main__":
     # Activate Bot
@@ -95,6 +159,7 @@ if __name__ == "__main__":
             updater.dispatcher.add_handler(
                 MessageHandler(Filters.text(telawa), reader_video_button_handler))
 
-    updater.dispatcher.add_handler(MessageHandler(Filters.text(BACK_BUTTON), start_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text(BACK_BUTTON), goBack))
 
+    data_source.create_tables()
     run()
